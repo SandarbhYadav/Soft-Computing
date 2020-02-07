6e6c16c1ad6026f6ecb6cb973d43725bf818e74a
SandarbhYadav <60778238+SandarbhYadav@users.noreply.github.com>
2020-02-07 21:54:43 +0530

    Add files via upload

---
+++ DE Ackley.cpp
@@ -0,0 +1,123 @@
+#include<bits/stdc++.h>
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+
+// ** CONTROL PARAMETERS ** //
+#define D 2       // dimension of problem
+#define NP 60 // size of population
+#define F 0.9 // differentiation constant
+#define CR 0.5 // crossover constant
+#define GEN 15 // number of generations
+#define L -2.048 // low boundary constraint
+#define H 2.048 // high boundary constraint
+
+using namespace std;
+
+double fnc(double* x)
+{
+	int i;
+	double s1 = 0;
+	double s2 = 0;
+	
+	for(i = 0; i < D; i++){
+		s1 += x[i]*x[i];
+		s2 += cos(6.14*x[i]);
+	}
+
+	return (-1.0)*20.0 * exp((-1.0) * 0.20 * sqrt((1.0/D) * s1)) - exp((1.0/D) * s2) + 2.71 + 20;
+}
+
+int main()
+{
+//***************************//
+//** ALGORITHM’S VARIABLES **//
+//***************************//
+double X[D] ; // trial vector
+double Pop[D][NP] ; // population
+double Fit[NP] ; // fitness of the population
+double f ; // fitness of the trial individual
+int iBest = 0 ; // index of the best solution
+int i,j,g ; // loop variables
+int Rnd ; // mutation parameter
+int r[3] ; // randomly selected indices
+
+//****************************//
+//** CREATION OF POPULATION **//
+//****************************//
+for (j=0; j<NP; j++) // initialize each individual
+{
+for (i=0; i<D; i++) // within boundary constraints
+Pop[i][j] = X [i] = L + (H-L)*((double)rand() / (double)RAND_MAX ) ;
+Fit[j] = fnc(X) ; // and evaluate fitness function
+}
+
+
+
+//******************//
+//** OPTIMIZATION **//
+//******************//
+for (g=0; g<GEN; g++) // for each generation
+{
+for (j=0; j<NP; j++) // for each individual
+{
+// choose three random individuals from population,
+// mutually different and also different from j
+r[0] = (int) (((double)rand() / (double)RAND_MAX )*NP) ;
+while (r[0]==j)
+r[0] = (int) (((double)rand() / (double)RAND_MAX )*NP) ;
+r[1] = (int) ((double)rand() / (double)RAND_MAX )*NP ;
+while ((r[1]==r[0])||(r[1]==j))
+r[1] = (int) (((double)rand() / (double)RAND_MAX )*NP) ;
+r[2] = (int) (((double)rand() / (double)RAND_MAX )*NP) ;
+while ((r[2]==r[1])||(r[2]==r[0])||(r[2]==j))
+r[2] = (int) (((double)rand() / (double)RAND_MAX )*NP) ;
+// create trial individual
+// in which at least one parameter is changed
+Rnd = (int)(((double)rand() / (double)RAND_MAX )*D) ;
+
+for (i=0; i<D; i++)
+{
+if ( (((double)rand() / (double)RAND_MAX )<CR) || (Rnd == i) )
+X[i] = Pop[i][r[2]] +
+F * (Pop[i][r[0]] - Pop[i][r[1]]) ;
+else
+X[i] = Pop[i][j] ;
+}
+// verify boundary constraints
+for (i=0; i<D; i++)
+if ((X[i]<L)||(X[i]>H))
+X[i] = L + (H-L)*((double)rand() / (double)RAND_MAX ) ;
+// select the best individual
+// between trial and current ones
+// evaluate fitness of trial individual
+f = fnc(X) ;
+// if trial is better or equal than current
+if (f <= Fit[j])
+{
+// replace current by trial
+for (i=0; i<D; i++)
+Pop[i][j] = X[i] ;
+Fit[j] = f ;
+// if trial is better than the best
+if (f <= Fit[iBest])
+iBest = j ; // update the best’s index
+}
+}
+}
+
+
+
+//*************//
+//** RESULTS **//
+//*************//
+printf("OPTIMUM : \n");
+for (i=0; i<D; i++)
+printf("%g\n",Pop[i][iBest]);
+printf("Fobj = %g\n",Fit[iBest]);
+scanf("%hd",&i);
+return 0;
+}
---
+++ PSO Rastrigin.cpp
@@ -0,0 +1,111 @@
+#include<iostream>
+#include<time.h>
+#include<stdlib.h>
+#include<math.h>
+#include<conio.h>
+#include<fstream>
+#include <iomanip>
+
+using namespace std ;
+const int n=3;                     // problem dimesion
+const int np=n+1;                  //fitness value
+const int tp=50;                // population size(swarm size)
+double pop[tp][np],x[n],l,U, vx[tp][n],pbest[tp][np],mini; //mini for gbest
+double fun(double []);
+int main()
+  {       
+    int i,j,best,iteration =0;
+    double w, r1, r2, c1 = 2.0, c2 = 2.0;  
+    l=-5;
+    U=5;
+  	iteration=0;
+  	srand(time(0)); //here time seed are used for generating random no.
+      
+    for(i=0;i<tp;i++)                            // initial population
+    {
+        for(j=0;j<n;j++)
+            {
+                x[j]=l+(U-l)*((float)rand()/RAND_MAX);
+                pop[i][j]=x[j];
+                vx[i][j] = l+(U-l)*((float)rand()/RAND_MAX);
+                pbest[i][j] = pop[i][j];
+            }
+        pbest[i][j] = pop[i][j] = fun(x);
+    }
+    
+    
+    mini = pbest[0][n];
+    best = 0;
+      
+      
+    for(i=0;i<tp;i++)                            
+    {
+        if(pbest[i][n]<mini)
+        {
+            mini = pbest[i][n];
+            best = i;
+        }
+    }    
+                          
+   while(iteration < 25)   //loop for No. of simulations
+   {
+       w=0.9-((0.9-0.4)/10)*iteration;  //Linearly Decreasing Inertia Weight
+       iteration++;
+       for(i=0;i<tp;i++)   //Loop for number of particles
+       {
+           for(j=0;j<n;j++)  //Loop for number of decision variables
+           {
+               r1=(float)rand()/RAND_MAX;  // r1 and r2 are U(0,1)
+               r2=(float)rand()/RAND_MAX;
+               vx[i][j] = w*vx[i][j] +c1*r1*(pbest[i][j] - pop[i][j]) +
+				                     c2*r2*(pbest[best][j] - pop[i][j]); //Velocity Update
+               pop[i][j] = pop[i][j] + vx[i][j];   //Position Update
+               while(pop[i][j]<l)   //Variable Bounds
+               {
+                   pop[i][j] = l;
+               }
+               while(pop[i][j]>U)
+               {
+                   pop[i][j] = U;
+               }
+           }
+       }
+       for(i =0; i<tp;i++)    //Choosing pbest
+       {
+           pop[i][n] = fun(pop[i]);
+           if(pop[i][n]<pbest[i][n])
+         {
+           for(j =0;j<=n;j++)
+           pbest[i][j] =pop[i][j];
+         }
+       if(pop[i][n]<pbest[best][n])  //Choosing gbest
+       best = i;
+       }
+	  
+	  cout<<iteration<<"\t";
+     cout<<fun(pbest[best]);
+     cout<<endl;
+   }
+
+   for(j=0; j<n; j++)					//Final Solution
+     cout<<pbest[best][j]<<"\t";
+   cout<<"\t";
+     cout<<fun(pbest[best]);
+
+    return 0;
+}                           
+                  
+   
+ //*************** Rastrigin function *******************//
+double fun(double x[])
+   {
+ 
+ int i;
+	double s1 = 0;
+	
+	for(i = 0; i < n; i++){
+		s1 += (x[i]*x[i]-10*cos(6.28*x[i]));
+	}
+	
+	return 10*n + s1;
+   }
